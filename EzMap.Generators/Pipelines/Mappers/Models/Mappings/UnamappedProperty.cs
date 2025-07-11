151725dff37852704fde45047229678c0579a906
Ludvig Rydahl <ludvig.rydahl+github@xlent.se>
2025-07-11 23:22:48 +0200

    adds better unmapped property reporting

--- EzMap.Generators/Pipelines/Mappers/Models/Mappings/UnamappedProperty.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.CodeAnalysis;
-
-namespace EzMap.Generators.Pipelines.Mappers.Models.Mappings
-{
-    internal abstract record UnamappedProperty(
-        IPropertySymbol? Symbol
-    ) : PropertyMapping;
-}
